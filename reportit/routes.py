bf2339e712ea38f683578cba2b9dfb56f4b6911f
Youssef-Harby <youssef_harby@yahoo.com>
2022-03-04 20:14:19 +0200

    TimeSeriesHeatmap

--- reportit/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, request
-from reportit import app
-from reportit.form import *
-
-@app.route('/')
-def index():
-    return render_template('form.html')
--- run.py
+++
@@ -1,5 +0,0 @@
-from reportit import app
-print('RUN')
-
-if __name__ == '__main__':
-    app.run(debug=True)
