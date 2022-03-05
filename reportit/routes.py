d896cb1c21d8f0683641e61911a91a203a49e7aa
Youssef-Harby <youssef_harby@yahoo.com>
2022-03-05 09:15:39 +0200

    add leafmap

--- reportit/routes.py
+++ reportit/routes.py
@@ -3,24 +3,35 @@ from reportit import app, df
 import folium
 from folium import plugins
 import geopandas
+import leafmap.kepler as leafmap
 from reportit.form import *
 
 @app.route('/')
 def index():
     return render_template('form.html')
 
-# print(df.info())
-geometry = geopandas.points_from_xy(df.lon, df.lat)
-geo_df = geopandas.GeoDataFrame(df[['fid','Description', 'lat', 'lon', 'timestamp']], geometry=geometry)
-# geo_df.head()
-# Create a geometry list from the GeoDataFrame
-geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry ]
-print(geo_df_list)
-
 @app.route('/folium')
 def maptest():
+    # print(df.info())
+    geometry = geopandas.points_from_xy(df.lon, df.lat)
+    geo_df = geopandas.GeoDataFrame(df[['fid','Description', 'lat', 'lon', 'timestamp']], geometry=geometry)
+    # geo_df.head()
+    # Create a geometry list from the GeoDataFrame
+    geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry ]
+    # print(geo_df_list)
+
     start_coords = (30.0444, 31.2357)
-    folium_map = folium.Map(location=start_coords, zoom_start=8)
+    folium_map = folium.Map(location=start_coords, zoom_start=6)
     plugins.HeatMap(geo_df_list).add_to(folium_map)
     # return df.keys()
     return folium_map._repr_html_()
+
+
+@app.route('/leafmap')
+def leafmapTest():
+    m = leafmap.Map(center=[30.0444, 31.2357], zoom=6, height=600, widescreen=False)
+    geometry = geopandas.points_from_xy(df.lon, df.lat)
+    geo_df = geopandas.GeoDataFrame(df[['fid','Description', 'lat', 'lon']], geometry=geometry)
+    m.add_gdf(geo_df, layer_name="Points", fill_colors=["red", "green", "blue"])
+    # m.to_html(outfile='./reportit/templates/leafmap.html')
+    return m._repr_html_()
