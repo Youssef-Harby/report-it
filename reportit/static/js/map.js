345ba38666f6d9d74661076bb5241d14a0541f8d
Khaled Nour <Khaled.mahmoud.nour0@gmail.com>
2022-03-06 18:32:24 +0200

    form with location and frorm data json

--- reportit/static/js/map.js
+++ reportit/static/js/map.js
@@ -1,16 +1,14 @@
-let map = L.map('map',{
-    center: [30.5, 30.9],
-    zoom: 20}
-);
+// let map = L.map('map',{
+//     center: [30.5, 30.9],
+//     zoom: 20}
+// );
 
-var myBaseMap =L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-myBaseMap.addTo(map)
+// var myBaseMap =L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(map);
+// myBaseMap.addTo(map)
 
-
-
-//------------------------------------------------------------
+// ------------------------------------------------------------------------
 
 // map.on("click", function (event) {
 //     console.log(  event.latlng.toString());
@@ -75,79 +73,124 @@ myBaseMap.addTo(map)
 //     });
 // } 
 
+// ---------------------------------------------
 
+// center, zoom, and maxZoom of the map
+var center = [52.45, 13.35],
+  zoom = 2,
+  moreZoom = 9,
+  maxZoom = 18
 
-
-
-// --------------------------------------------- 
-
-map.locate({setView: true, maxZoom: 100});
-
-
-
-function onLocationFound(e) {
-    
-    var radius = e.accuracy / 2;
-    L.marker(e.latlng).addTo(map)
-      .bindPopup("Your Location").openPopup();
-    L.circle(e.latlng, radius).addTo(map);
-  }
-  
-  map.on('locationfound', onLocationFound);
-  map.locate({setView: true,  maxZoom: 100});
-
-
-
-// -------------------------------------------
-
-
-function geoFindMe() {
-
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
-
-  mapLink.href = '';
-  mapLink.textContent = '';
-
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    cordJson=(JSON.stringify({
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          position.coords.latitude, position.coords.longitude
-        ]
-      }
-    }))
-
-  console.log(cordJson)
-
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-  }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if(!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-
-  
-
+// get location using the Geolocation interface
+var geoLocationOptions = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+  maximumAge: 0
 }
 
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
+var myLat, myLng, display_name, myMarker
 
+function cb(data) {
+  display_name = data.display_name
+  myMarker.addTo(map)
+    .bindPopup(`<b>Your location</b><br>
+                   Latitude: ${myLat} <br>
+                   Longitude: ${myLng} <br> 
+                <b>Your address</b><br>
+                   ${display_name} <br
+                 `).openPopup()
+                 document.getElementById("demo").innerHTML = display_name;
+}
+
+function success(position) {
+
+  myLat = position.coords.latitude.toFixed(6)
+  myLng = position.coords.longitude.toFixed(6)
+  latLng = [myLat, myLng]
+  map.setZoom(200)
+  map.panTo(latLng)
+
+  cordJson=(JSON.stringify({
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        position.coords.latitude, position.coords.longitude
+      ]
+    }
+  }))
+
+console.log(cordJson)
+
+  var script = document.createElement('script')
+  script.id = 'nominatim'
+  script.async = true // This is required for asynchronous execution
+  script.src = 'https://nominatim.openstreetmap.org/reverse?json_callback=cb&format=json&lat=' + myLat + '&lon=' +
+    myLng + '&zoom=27&addressdetails=1'
+  document.body.appendChild(script)
+  myMarker = L.marker(latLng)
+  document.body.removeChild(script)
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`)
+}
+
+navigator.geolocation.getCurrentPosition(success, error, geoLocationOptions)
+
+
+
+// create the map
+var map = L.map('map', {
+  contextmenu: true,
+  contextmenuWidth: 140,
+  contextmenuItems: [{
+      text: 'Center map here',
+      callback: centerMap
+    },
+    {
+      text: 'Add marker here',
+      callback: addMarker
+    }
+  ]
+}).setView(center, zoom)
+
+
+
+// set up the OSM layer
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: maxZoom
+  }).addTo(map)
+
+
+// function to center map
+function centerMap(e) {
+  map.panTo(e.latlng)
+}
+
+// function to add marker
+function addMarker(e) {
+  L.marker(e.latlng).addTo(map)
+}
+
+
+
+// ------------------------------------------------------------------------------- 
+
+// map.locate({setView: true, maxZoom: 100});
+
+// function onLocationFound(e) {
+    
+//     var radius = e.accuracy / 2;
+//     L.marker(e.latlng).addTo(map)
+//       .bindPopup("Your Location").openPopup();
+//     L.circle(e.latlng, radius).addTo(map);
+//   }
+  
+//   map.on('locationfound', onLocationFound);
+//   map.locate({setView: true,  maxZoom: 100});
 
 //------------------------------------------------------------------
 
@@ -166,12 +209,15 @@ function submitForm(event) {
   formData.forEach((value, key) => (data[key] = value));
   // Log the data.
   console.log(data);
+ 
 }
-
-
-
 // ------------------------------------------------------------
 
-
+// window.onload = function() {
+//   geoFindMe();
+// };
 
 // -----------------------------------------------------------
+
+
+
