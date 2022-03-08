47f368e552f190883af7a58ffb1a305e59ef1e28
Khaled Nour <Khaled.mahmoud.nour0@gmail.com>
2022-03-08 16:20:41 +0200

    Drobdown modified

--- reportit/static/js/map.js
+++ reportit/static/js/map.js
@@ -111,7 +111,7 @@ function submitForm(event) {
   const url = "http://localhost:5000/jsontest";
   const formData = new FormData(event.target);
   // Build the data object.
-  const data = { lat: myLat, lng: myLng, DateTime: dateTime ,markerLat: markerLat, markerLng :markerLng };
+  const data = { lat: myLat, lng: myLng };
  
   formData.forEach((value, key) => (data[key] = value));
   // Log the data.
@@ -157,140 +157,127 @@ function encodeImageFileAsURL(element) {
   reader.readAsDataURL(file);
 }
 
-
-// --------------------------------date time------------------------------------
-var dateTime
-//Pad given value to the left with "0"
-function AddZero(num) {
-  return (num >= 0 && num < 10) ? "0" + num : num + "";
-}
-window.onload = function() {
-  var now = new Date();
-  var strDateTime = [[AddZero(now.getDate()), 
-      AddZero(now.getMonth() + 1), 
-      now.getFullYear()].join("/"), 
-      [AddZero(now.getHours()), 
-      AddZero(now.getMinutes())].join(":"), 
-      now.getHours() >= 12 ? "PM" : "AM"].join(" ");
-  dateTime=strDateTime
-};
 // ----------------------------------------------------------------------------------
 
 
 
 // --------------------------------------Marker Manually--------------------------
-var marker = null;
-var markerLat
-var markerLng
-map.on('click', function (e) {
-    if (marker !== null) {
-        map.removeLayer(marker);
-    }
-    marker = L.marker(e.latlng).addTo(map);
-    markerCord=e.latlng
-    lat="lat"
-    markerLat=markerCord[lat]
-    lng="lng"
-    markerLng=markerCord[lng]
-});
+// var marker = null;
+// var markerLat
+// var markerLng
+// map.on('click', function (e) {
+//     if (marker !== null) {
+//         map.removeLayer(marker);
+//     }
+//     marker = L.marker(e.latlng).addTo(map);
+//     markerCord=e.latlng
+//     lat="lat"
+//     markerLat=markerCord[lat]
+//     lng="lng"
+//     markerLng=markerCord[lng]
+// });
 
 
 // <!-- --------------------------------Doamin/Subtype----------------------------------------------- -->
               
-                var Utility =["Gas","Water","Sewage","Electric","Telecommunication"];
-                var Poullution = ["Noise Pollution","Air Pollution","Industrial Pollution","Soil Pollution","Water Pollution"];
-                var Road = ["Accidents","Lamps","Hales","Barriers"];
-                var Disasters = ["Earthquakes","Flloods","Landslides","Torrnados"];
+var Utility = ["Gas","Water","Sewage","Electric","Telecommunication"];
+var Poullution = ["Noise Pollution","Air Pollution","Industrial Pollution","Soil Pollution","Water Pollution"];
+var Road = ["Accidents","Lamps","Hales","Barriers"];
+var Disasters = ["Earthquakes","Flloods","Landslides","Torrnados"];
 
-                // --------------------------Sub subUtility-----------------------------
-                    var Water= ["Pipeline Break","Leakage","Poor Quality","Weak Pressure"];
-                    var Sewage = ["Leakage","Cover theft"]; 
+// --------------------------Sub subUtility-----------------------------
+    var Water= ["Pipeline Break","Leakage","Poor Quality","Weak Pressure"];
+    var Sewage = ["Leakage","Cover theft"]; 
 
-                // ---------------------------------------------------------------------
-                
-                function updateTwo() {
-                    var  problem = document.getElementById("1");
-                    var subProblem = document.getElementById("2");
-                    var selected = problem.options[problem.selectedIndex].value;
-                    var i;
-                    for(i = subProblem.options.length - 1 ; i >= 0 ; i--) {
-                        subProblem.remove(i);
-                    }
-                    if(selected=="Utility"){
-                        for(var i = 0; i < Utility.length; i++) {
-                            addOption(subProblem,Utility[i]);
-                        }
-                    } else if(selected=="Poullution") {
-                        for(var j = 0; j < Poullution.length; j++) {
-                            addOption(subProblem,Poullution[j]);
-                        }
-                    } else if(selected=="Road") {
-                        for(var k = 0; k < Road.length; k++) {
-                            addOption(subProblem,Road[k]);
-                        }
+// ---------------------------------------------------------------------
 
-                    } else if(selected=="Disasters") {
-                        for(var m = 0; m < Disasters.length; m++) {
-                            addOption(subProblem,Disasters[m]);
-                        }
-                    }
+function updateTwo() {
+    var  problem = document.getElementById("1");
+    var subProblem = document.getElementById("2");
+    var selected = problem.options[problem.selectedIndex].value;
+    var SubUtility = document.getElementById("3");
+    var i;
+    for(i = subProblem.options.length - 1 ; i >= 0 ; i--) {
+        subProblem.remove(i);
+        SubUtility.remove(i);
+    }
+    if(selected=="Utility"){
+        for(var i = 0; i < Utility.length; i++) {
+            addOption(subProblem,Utility[i]);
+        }
+    } else if(selected=="Poullution") {
+        for(var j = 0; j < Poullution.length; j++) {
+            addOption(subProblem,Poullution[j]);
+        }
+    } else if(selected=="Road") {
+        for(var k = 0; k < Road.length; k++) {
+            addOption(subProblem,Road[k]);
+        }
+
+    } else if(selected=="Disasters") {
+        for(var m = 0; m < Disasters.length; m++) {
+            addOption(subProblem,Disasters[m]);
+        }
+    }
 
 
-                    var dropdown = document.getElementById("1");
-                    var current_value = dropdown.options[dropdown.selectedIndex].value;
-                
-                        if (current_value == "Utility") {
-                            document.getElementById("3").style.display = "block";
-                        }
-                        else {
-                            document.getElementById("3").style.display = "none";
-                        }
+    
+        var dropdown = document.getElementById("1");
+        var current_value = dropdown.options[dropdown.selectedIndex].value;
+    
+        if (current_value == "Utility") {
+            document.getElementById("3").style.display = "block";
+        }
+        else {
+            document.getElementById("3").style.display = "none";
+        }
+   
+ 
+}
 
-                }
-                
-                updateThree = function() {
-                    var  subProblem = document.getElementById("2");
-                    var subUtility = document.getElementById("3");
-                    var selected = subProblem.options[subProblem.selectedIndex].value;
-                    var i;
-
-                    // ---------------------------------Sub subUtility--------------------------------------
-                                            for(i = subUtility.options.length - 1 ; i >= 0 ; i--) {
-                                              subProblem.remove(i);
-                                                    subUtility.remove(i);
-                                                }
-                                                if(selected=="Water"){
-                                                    for(var i = 0; i < Water.length; i++) {
-                                                        addOption(subUtility,Water[i]);
-                                                    }
-                                                } else if(selected=="Sewage") {
-                                                    for(var j = 0; j < Sewage.length; j++) {
-                                                        addOption(subUtility,Sewage[j]);
-                                                    }
-                                                } 
-                                
-
-                                            var dropdown = document.getElementById("2");
-                                            var current_value = dropdown.options[dropdown.selectedIndex].value;
-                                                
-                                                if (current_value == "Water" || current_value == "Sewage" ) {
-                                                  document.getElementById("3").style.display = "block";
-                                                }
-                                        
-                                                else {
-                                                    document.getElementById("3").style.display = "none";
-                                                }
+updateThree = function() {
+    var  subProblem = document.getElementById("2");
+    var subUtility = document.getElementById("3");
+    var selected = subProblem.options[subProblem.selectedIndex].value;
+    var i;
+        
+    // ---------------------------------Sub subUtility--------------------------------------
+                            for(i = subUtility.options.length - 1 ; i >= 0 ; i--) {
+                                    subUtility.remove(i);
+                                }
+                                if(selected=="Water"){
+                                    for(var i = 0; i < Water.length; i++) {
+                                        addOption(subUtility,Water[i]);
                                     }
-                    
+                                } else if(selected=="Sewage") {
+                                    for(var j = 0; j < Sewage.length; j++) {
+                                        addOption(subUtility,Sewage[j]);
+                                    }
+                                } 
 
-                addOption = function(option, value) {
-                    var opt = document.createElement('option');
-                    opt.innerHTML = value;
-                    opt.value = value;
-                    option.appendChild(opt);
-                }
-                
-                window.onload = function() {
-                  updateTwo(),updateThree()
-                };
- // <!-- -------------------------------------------------------------------------------------------------- -->
+                            var dropdown = document.getElementById("2");
+                            var current_value = dropdown.options[dropdown.selectedIndex].value;
+                            
+                                if (current_value == "Water" || current_value == "Sewage" ) {
+                                    document.getElementById("3").style.display = "block";
+                                }
+                           
+                                else {
+                                    document.getElementById("3").style.display = "none";
+                                }
+
+                    }
+    
+
+addOption = function(option, value) {
+    var opt = document.createElement('option');
+    opt.innerHTML = value;
+    opt.value = value;
+    option.appendChild(opt);
+    
+}
+
+// <!-- -------------------------------------------------------------------------------------------------- -->
+// window.onload = function() {
+// updateTwo(),updateThree()
+// };
