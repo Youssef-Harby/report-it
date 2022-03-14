a7962b669854fc29f9c5eab822e2fde45685102c
Youssef-Harby <youssef_harby@yahoo.com>
2022-03-15 01:56:07 +0200

    add more dummy data

--- reportit/analysis/sjoina.py
+++ reportit/analysis/sjoina.py
@@ -10,16 +10,17 @@ def sJoinA(sqlQ):
     Problem_gdf = Problem_gdf.to_crs("EPSG:3857")
 
     #Facilities Layer From GPKG or PostGIS
-    facilities = geopandas.read_file("Data/Facilities/DemoCairo.gpkg", layer='facilities').to_crs("EPSG:3857") #Point
+    facilities = geopandas.read_file("Data/Facilities/EgyptFacilities.gpkg", layer='traffic').to_crs("EPSG:3857") #Point
 
     #Service Area Polygon
-    admin_poly = geopandas.read_file("Data/Facilities/DemoCairo.gpkg", layer='NewCairoPolyDemo').to_crs("EPSG:3857") #Polygon
+    admin_poly = geopandas.read_file("Data/Facilities/Admin3Poly.gpkg", layer='All-Admin-Area-Egypt').to_crs("EPSG:3857") #Polygon
 
     #Spatial Join (Facilities and Service Area Polygon)
     fac_And_Admin = geopandas.sjoin(facilities,admin_poly[['adm3_ar', 'geometry']], how='left')
     fac_And_Admin.rename_geometry('fac_geo', inplace=True)
-    type_name = ['Fire','Health','Road','Utility']
-    fac_And_Admin_for_Problem = fac_And_Admin.loc[fac_And_Admin['type']==type_name[0]]
+    # type_name = ['Fire','Health','Road','Utility']
+    # fac_And_Admin_for_Problem = fac_And_Admin.loc[fac_And_Admin['fclass']=='hospital']
+    fac_And_Admin_for_Problem = fac_And_Admin
 
     #Add lat and lon from geometry column
     fac_And_Admin_for_Problem = fac_And_Admin_for_Problem.to_crs("EPSG:4326")
@@ -29,7 +30,7 @@ def sJoinA(sqlQ):
     #
     problem_And_Admin = geopandas.sjoin(Problem_gdf,admin_poly[['adm3_ar', 'geometry']], how='left')
 
-    final_result = problem_And_Admin.merge(fac_And_Admin_for_Problem[['fac_lat','fac_lon','adm3_ar']], on='adm3_ar',how='left').to_crs("EPSG:4326")
+    final_result = problem_And_Admin.merge(fac_And_Admin_for_Problem[['fac_lat','fac_lon','adm3_ar','email','phone']], on='adm3_ar',how='left').to_crs("EPSG:4326")
     final_result['lat'] = final_result['geometry'].y
     final_result['lon'] = final_result['geometry'].x
 
